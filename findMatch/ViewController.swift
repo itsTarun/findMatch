0acb17512c96ff89d028baf4d49c04b9a020715a
Tarun <itstarun1994@gmail.com>
2019-05-02 23:59:57 +0530

    Card View Drag/Drop with pan gesture

--- findMatch/ViewController.swift
+++ findMatch/ViewController.swift
@@ -5,35 +5,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     let topStackView = HomeTopStackView()
-    let centerView = UIView()
-    let bottomStackView = HomeBottomStackView()
+    let cardsDeckView = UIView()
+    let buttonsStackView = HomeBottomStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupLayout()
+        setupDummyCards()
     }
+    
 
+    
 }
 
 extension ViewController {
     
-    fileprivate func setupLayout() {
-        
-        centerView.backgroundColor = .blue
-        
-        let overAllStackView = UIStackView(arrangedSubviews: [topStackView, centerView, bottomStackView])
-        
-        overAllStackView.axis = .vertical
-        
-        view.addSubview(overAllStackView)
-        
-        overAllStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                                leading: view.leadingAnchor,
-                                bottom: view.safeAreaLayoutGuide.bottomAnchor,
-                                trailing: view.trailingAnchor)
+    // MARK:- Fileprivate
+    
+    fileprivate func setupDummyCards() {
+        print("Setting up dummy cards")
+        let cardView = CardView(frame: .zero)
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
     }
+    
+    
+    
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
+        overallStackView.axis = .vertical
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckView)
+    }
+    
     
 }
