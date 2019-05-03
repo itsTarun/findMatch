b5010e64ed4bfa458761730843ea6b1873bc3988
NMahendroo <naman.mahendroo@startxlabs.com>
2019-05-03 16:36:23 +0530

    Keyboard Management Notification Observers

--- findMatch/Controller/RegistrationViewController.swift
+++ findMatch/Controller/RegistrationViewController.swift
@@ -1,91 +1,128 @@
-//
-//  RegistrationViewController.swift
-//  findMatch
-
 
 import UIKit
 
 class RegistrationViewController: UIViewController {
     
     // UI Components
-    
-    let selectPhotoButton : UIButton = {
-        let button = UIButton(type: UIButton.ButtonType.system)
-        
+    let selectPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
         button.setTitle("Select Photo", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
         button.backgroundColor = .white
         button.setTitleColor(.black, for: .normal)
-        button.heightAnchor.constraint(equalToConstant: 220).isActive = true
-        
+        button.heightAnchor.constraint(equalToConstant: 275).isActive = true
         button.layer.cornerRadius = 16
-        
         return button
     }()
     
-    let fullNameTextField: UITextField = {
-       let tf = CustomTextField(padding: 24)
+    let fullNameTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24, height: 44)
         tf.placeholder = "Enter full name"
         tf.backgroundColor = .white
         return tf
     }()
-    
-    let emailTextField: UITextField = {
-        let tf = CustomTextField(padding: 24)
+    let emailTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24, height: 44)
         tf.placeholder = "Enter email"
         tf.keyboardType = .emailAddress
         tf.backgroundColor = .white
         return tf
     }()
-    
-    let passwordTextField: UITextField = {
-        let tf = CustomTextField(padding: 24)
+    let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24, height: 44)
         tf.placeholder = "Enter password"
         tf.isSecureTextEntry = true
         tf.backgroundColor = .white
         return tf
     }()
     
-    
-
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
+        button.backgroundColor = #colorLiteral(red: 0.8235294118, green: 0, blue: 0.3254901961, alpha: 1)
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.layer.cornerRadius = 22
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupGradientLayer()
+        setupLayout()
+        setupNotificationObservers()
+        setupTapGesture()
+    }
+    
+    // MARK:- Private
+    
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        self.view.endEditing(true) // dismisses keyboard
+    }
+    
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self) // you'll have a retain cycle
+    }
+    
+    @objc fileprivate func handleKeyboardHide() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.transform = .identity
+        })
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        // how tall the keyboard actually is
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardFrame = value.cgRectValue
+        // print(keyboardFrame)
         
-        let stackView = UIStackView(arrangedSubviews: [selectPhotoButton,fullNameTextField,emailTextField,passwordTextField])
-        stackView.axis = .vertical
-        stackView.spacing = 10
+        // try to figure out how tall the gap is from the register button to the bottom of the screen
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        // print(bottomSpace)
         
-        view.addSubview(stackView)
+        let difference = keyboardFrame.height - bottomSpace
         
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: UIEdgeInsets(top: 0, left: 32, bottom: 0, right: 32))
+        self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
         
-        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
     }
     
-}
-
-
-extension RegistrationViewController {
+    lazy var stackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton,
+        fullNameTextField,
+        emailTextField,
+        passwordTextField,
+        registerButton
+        ])
     
-    fileprivate func setupGradientLayer()
-    {
+    fileprivate func setupLayout() {
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    fileprivate func setupGradientLayer() {
         let gradientLayer = CAGradientLayer()
-        
-        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3647058824, blue: 0.3568627451, alpha: 1)
-        
-        let bottomColor = #colorLiteral(red: 0.9173635563, green: 0.2775638204, blue: 0.4196078431, alpha: 1)
-        
+        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
+        let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
+        // make sure to cgColor
         gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
-        
-        gradientLayer.locations = [0,1]
-        
+        gradientLayer.locations = [0, 1]
         view.layer.addSublayer(gradientLayer)
-        
         gradientLayer.frame = view.bounds
     }
     
--- findMatch/Views/CustomTextField.swift
+++ findMatch/Views/CustomTextField.swift
@@ -8,23 +8,25 @@ import UIKit
 class CustomTextField: UITextField {
     
     let padding: CGFloat
+    let height: CGFloat
     
-    init(padding: CGFloat) {
+    init(padding: CGFloat, height: CGFloat) {
         self.padding = padding
+        self.height = height
         super.init(frame: .zero)
-        layer.cornerRadius = 25
+        layer.cornerRadius = height / 2
     }
     
-    override var intrinsicContentSize: CGSize {
-        return .init(width: 0, height: 50)
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
     }
     
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.insetBy(dx: padding, dy: 0)
     }
     
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.insetBy(dx: padding, dy: 0)
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: height)
     }
     
     required init?(coder aDecoder: NSCoder) {
